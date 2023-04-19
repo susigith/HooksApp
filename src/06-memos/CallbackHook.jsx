10f35b3fbe34d32e1cf6c7afd0880228f82d65a3
Susi <102126517+susigith@users.noreply.github.com>
2023-04-19 07:49:06 -0400

    useCallback Hook

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,26 @@
+import { useCallback, useEffect, useState } from 'react';
+import { ShowIncrement } from './ShowIncrement';
+
+export const CallbackHook = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementFather = useCallback(() => {
+    setCounter((counter) => counter + 1);
+  }, []);
+
+  //   useEffect(() => {
+  //     incrementFather();
+  //   }, [incrementFather]);
+
+  //   const incrementFather = () => {
+  //     setCounter(counter + 1);
+  //   };
+
+  return (
+    <>
+      <h1>Callback Hook: {counter} </h1>
+      <hr />
+      <ShowIncrement increment={incrementFather} />
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import ReactDOM from 'react-dom/client';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
-import { MemoHook } from './06-memos/MemoHook';
+// import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 
 import './index.css';
 
@@ -24,6 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <FocusScreen />
   // <Layout />
   // <Memorize />
-  <MemoHook />
+  // <MemoHook />
+  <CallbackHook />
   // </React.StrictMode>
 );
