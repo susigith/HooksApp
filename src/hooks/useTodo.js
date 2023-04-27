acbf2843ab041f4526a16ee9eb097683038265e6
Susi <102126517+susigith@users.noreply.github.com>
2023-04-27 07:11:19 -0400

    Custom useTodo Hook modified

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,11 +1,11 @@
 import { useEffect, useReducer } from 'react';
 import { todoReducer } from '../08-useReducer';
 
-export const useTodo = () => {
-  const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || [];
-  };
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
 
+export const useTodo = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
 
   useEffect(() => {
