40bb94a91f171f570b7456228f3b640b867001ff
Susi <102126517+susigith@users.noreply.github.com>
2023-04-28 18:54:51 -0400

    React Router - add <NavLink /> modified

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -4,7 +4,6 @@ import { Navbar } from './Navbar';
 export const MainApp = () => {
   return (
     <>
-      <h1>MainApp</h1>
       <Navbar />
       <hr />
       <div>
--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark rounded-3'>
       <div className='container-fluid'>
         <Link className='navbar-brand' to='/'>
           useContext
