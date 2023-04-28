363fc9f4355207db1e7bda6e676d6900ae0c9a6a
Susi <102126517+susigith@users.noreply.github.com>
2023-04-28 11:57:19 -0400

    React Router - add <Link />

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import { Navbar } from './Navbar';
 
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      <Navbar />
       <hr />
       <div>
         <Outlet />
