445e00a13ab7a48be6f7122142c1623b5eb7190a
Susi <102126517+susigith@users.noreply.github.com>
2023-04-29 19:11:36 -0400

    add useContext

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -1,8 +1,14 @@
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
+
 export const HomePage = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <>
-      <h1>HomePage</h1>
+      <h1>HomePage {user?.name} </h1>
       <hr />
+      <pre> {JSON.stringify(user, null, 2)} </pre>
     </>
   );
 };
